a0cde5ab563a591398c6187e23d0d052394c0f45
Tilo Himmelsbach <dertilo@gmail.com>
2020-11-09 22:41:03 +0100

    wtf

--- relatoria_consejodeestado.py
+++ relatoria_consejodeestado.py
@@ -1,17 +1,22 @@
+import itertools
 import os
+import shutil
 from datetime import timedelta, date
 from typing import Tuple
 
 import pandas
+from selenium.webdriver.chrome.webdriver import WebDriver
+from tqdm import tqdm
 from util import data_io
 from selenium import webdriver
 from bs4 import BeautifulSoup
 
 
-def build_chrome_driver(data_path):
+def build_chrome_driver(dir:str):
+    os.makedirs(dir, exist_ok=True)
     options = webdriver.ChromeOptions()
     options.add_argument("headless")
-    prefs = {"download.default_directory": data_path}
+    prefs = {"download.default_directory": dir}
     options.add_experimental_option("prefs", prefs)
 
     driver = webdriver.Chrome(
@@ -58,7 +63,7 @@ def fmt(x):
     return str(x) if len(str(x))==2 else f"0{str(x)}"
 
 
-def scrape_date_range(date_from:date,date_to:date):
+def scrape_date_range(date_from:date,date_to:date,wd,data_path):
     year_from = date_from.year
     month_from = date_from.month
     day_from = date_from.day
@@ -67,27 +72,50 @@ def scrape_date_range(date_from:date,date_to:date):
     month_to = date_to.month
     day_to = date_to.day
 
-    page = 0
+    date_range = f"{year_from}-{month_from}-{date_from}_{year_to}-{month_to}-{day_to}"
+    for page in itertools.count(start=0):
+        bucket_path = f"{data_path}/{date_range}_{page}"
+        url = f"{base_url}/?de=0&se=0&ac=0&ca=0&rs={year_from}%2F{fmt(month_from)}%2F{fmt(day_from)}&re={year_to}%2F{fmt(month_to)}%2F{fmt(day_to)}&st=0&pg={page}"
+        bucket_downloads = f"{bucket_path}/downloads"
+        os.makedirs(bucket_downloads, exist_ok=True)
 
-    bucket_name = f"from_{year_from}_{month_from}_{date_from}-to_{year_to}_{month_to}_{day_to}-page_{page}"
-    data_path = f"{os.environ['HOME']}/data/relatoria_consejodeestado/{bucket_name}"
-    os.makedirs(data_path, exist_ok=True)
-    url = f"{base_url}/?de=0&se=0&ac=0&ca=0&rs={year_from}%2F{fmt(month_from)}%2F{fmt(day_from)}&re={year_to}%2F{fmt(month_to)}%2F{fmt(day_to)}&st=0&pg={page}"
-    wd = build_chrome_driver(data_path)
-    wd.get(url)
-    g = get_hits(wd.page_source)
-    data_io.write_jsonl(f"{data_path}/docs.jsonl", (build_doc(wd, hit) for hit in g))
+        docs_file = f"{bucket_path}/docs.jsonl"
+        if os.path.isfile(docs_file):
+            continue
+
+        wd.get(url)
+        source = wd.page_source
+        if "No hay resultados para mostrar en este momento" in source:
+            break
+
+        docs = []
+        for hit in get_hits(source):
+            docs.append(build_doc(wd, hit))
+            yield True
+
+        data_io.write_jsonl(docs_file,docs)
+
+        for file_name in os.listdir(download_path):
+            shutil.move(os.path.join(download_path, file_name), bucket_downloads)
 
 
 if __name__ == "__main__":
 
     base_url = "http://relatoria.consejodeestado.gov.co"
+    data_path = f"{os.environ['HOME']}/data/relatoria_consejodeestado"
+    download_path = f"{data_path}/downloads"
+    wd = build_chrome_driver(download_path)
 
 
-    dates = pandas.date_range("2010", "2020", freq="1M")+timedelta(days=1)
+    dates = list(reversed(pandas.date_range("2010", "2020", freq="1M")+timedelta(days=1)))
     jobs = [
         (d1.date(), (d2 - timedelta(days=1)).date())
         for d1, d2 in zip(dates[:-1], dates[1:])
     ]
+    os.listdir(data_path)
+    g = (_ for s,e in jobs for _ in scrape_date_range(s,e,wd,data_path))
 
-    scrape_date_range(jobs[0])
+    for _ in tqdm(g):
+        pass
+
+

c87bc945da43f8db0cbe9427f0a0653316fd597b
Tilo Himmelsbach <dertilo@gmail.com>
2020-11-09 21:45:33 +0100

    scrape_date_range

--- relatoria_consejodeestado.py
+++ relatoria_consejodeestado.py
@@ -1,14 +1,17 @@
 import os
+from datetime import timedelta, date
+from typing import Tuple
 
+import pandas
 from util import data_io
 from selenium import webdriver
 from bs4 import BeautifulSoup
 
 
-def build_chrome_driver():
+def build_chrome_driver(data_path):
     options = webdriver.ChromeOptions()
     options.add_argument("headless")
-    prefs = {"download.default_directory": download_path}
+    prefs = {"download.default_directory": data_path}
     options.add_experimental_option("prefs", prefs)
 
     driver = webdriver.Chrome(
@@ -34,45 +37,57 @@ def get_hits(page: str):
     return g
 
 
+def build_doc(wd,hit):
+    wd.get(f"{base_url}/{hit['path']}")
+    soup = BeautifulSoup(wd.page_source, features="html.parser")
+    if (
+        "Lo sentimos mucho pero ese archivo todavía no está disponible para la página"
+        in soup.text
+    ):
+        element = wd.find_element_by_xpath("/html/body/do/div/div/div/p[2]/a")
+        element.click()
+        # path = soup.find_all("p")[-1].find_all("a")[0].attrs["href"]
+        # url = f"{base_url}/Document/{path}"
+        doc = {"redirect": soup.text}
+    else:
+        doc = {"page": soup.text}
+    doc.update(hit)
+    return doc
+
+def fmt(x):
+    return str(x) if len(str(x))==2 else f"0{str(x)}"
+
+
+def scrape_date_range(date_from:date,date_to:date):
+    year_from = date_from.year
+    month_from = date_from.month
+    day_from = date_from.day
+
+    year_to = date_to.year
+    month_to = date_to.month
+    day_to = date_to.day
+
+    page = 0
+
+    bucket_name = f"from_{year_from}_{month_from}_{date_from}-to_{year_to}_{month_to}_{day_to}-page_{page}"
+    data_path = f"{os.environ['HOME']}/data/relatoria_consejodeestado/{bucket_name}"
+    os.makedirs(data_path, exist_ok=True)
+    url = f"{base_url}/?de=0&se=0&ac=0&ca=0&rs={year_from}%2F{fmt(month_from)}%2F{fmt(day_from)}&re={year_to}%2F{fmt(month_to)}%2F{fmt(day_to)}&st=0&pg={page}"
+    wd = build_chrome_driver(data_path)
+    wd.get(url)
+    g = get_hits(wd.page_source)
+    data_io.write_jsonl(f"{data_path}/docs.jsonl", (build_doc(wd, hit) for hit in g))
+
+
 if __name__ == "__main__":
 
     base_url = "http://relatoria.consejodeestado.gov.co"
-    year_from = 1900
-    year_to = 2019
-    month_to = 11
-    day_to = 16
-    page = 0
 
-    folder_name = f"from_{year_from}-to_{year_to}_{month_to}_{day_to}-page_{page}"
-    download_path = (
-        f"{os.environ['HOME']}/data/relatoria_consejodeestado_downloads/{folder_name}"
-    )
-    os.makedirs(download_path, exist_ok=True)
 
-    url = f"{base_url}/?de=0&se=0&ac=0&ca=0&rs={year_from}%2F01%2F01&re={year_to}%2F{month_to}%2F{day_to}&st=0&pg={page}"
+    dates = pandas.date_range("2010", "2020", freq="1M")+timedelta(days=1)
+    jobs = [
+        (d1.date(), (d2 - timedelta(days=1)).date())
+        for d1, d2 in zip(dates[:-1], dates[1:])
+    ]
 
-    wd = (
-        build_chrome_driver()
-    )  # you can give three options here 1.Firefox 2.ChromeDriver 3.PhantomJS
-    wd.get(url)
-
-    g = get_hits(wd.page_source)
-
-    def build_doc(hit):
-        wd.get(f"{base_url}/{hit['path']}")
-        soup = BeautifulSoup(wd.page_source, features="html.parser")
-        if (
-            "Lo sentimos mucho pero ese archivo todavía no está disponible para la página"
-            in soup.text
-        ):
-            element = wd.find_element_by_xpath("/html/body/do/div/div/div/p[2]/a")
-            element.click()
-            # path = soup.find_all("p")[-1].find_all("a")[0].attrs["href"]
-            # url = f"{base_url}/Document/{path}"
-            doc = {"redirect": soup.text}
-        else:
-            doc = {"page": soup.text}
-        doc.update(hit)
-        return doc
-
-    data_io.write_jsonl("docs.jsonl", (build_doc(hit) for hit in g))
+    scrape_date_range(jobs[0])

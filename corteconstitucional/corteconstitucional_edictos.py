09cc459b413e7f49cee8b3c269cf2b21ce018b85
Tilo Himmelsbach <dertilo@gmail.com>
2020-12-16 18:17:00 +0100

    parse_pdfs

--- corteconstitucional/corteconstitucional_edictos.py
+++ corteconstitucional/corteconstitucional_edictos.py
@@ -1,12 +1,17 @@
 import os
+import re
+from pathlib import Path
 
+import textract
 from bs4 import BeautifulSoup
 from tqdm import tqdm
+from util import data_io
+from util.util_methods import exec_command
 
 from common import build_chrome_driver
 
 
-def download_edictos_pdfs(
+def download_edictos(
     download_path=f"{os.environ['HOME']}/data/corteconstitucional/edictos",
 ):
     url = "https://www.corteconstitucional.gov.co/secretaria/edictos/"
@@ -25,6 +30,20 @@ def download_edictos_pdfs(
     148
     """
 
+def parse_pdfs():
+    download_path=f"{os.environ['HOME']}/data/corteconstitucional/edictos"
+    re.compile(r"expediente ")
+    for file in Path(download_path).rglob("*.pdf"):
+        pdf_file = f"{download_path}/{file.name}"
+        text = textract.process(pdf_file)
+        match = re.compile(r"expediente\s{1,10}\w{1,5}-\d{1,10}").search(text.decode("utf-8")).group()
+        eid = re.sub(r"expediente\s{1,10}","",match)
+        # html_lines = exec_command(f"pdftohtml -noframes -stdout '{pdf_file}'")["stdout"]
+        # html = "\n".join([l.decode("utf-8") for l in html_lines])
+        # soup = BeautifulSoup(html, features="html.parser")
+        yield eid
+
+
 
 if __name__ == "__main__":
-    download_edictos_pdfs()
+    download_edictos()

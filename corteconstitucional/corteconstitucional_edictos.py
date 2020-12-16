09004cfeccee30e5941764c5262b833e9396bbf9
Tilo Himmelsbach <dertilo@gmail.com>
2020-12-16 16:53:18 +0100

    download_edictos_pdfs

--- corteconstitucional/corteconstitucional_edictos.py
+++ corteconstitucional/corteconstitucional_edictos.py
@@ -5,24 +5,26 @@ from tqdm import tqdm
 
 from common import build_chrome_driver
 
-if __name__ == '__main__':
+
+def download_edictos_pdfs(
+    download_path=f"{os.environ['HOME']}/data/corteconstitucional/edictos",
+):
     url = "https://www.corteconstitucional.gov.co/secretaria/edictos/"
-
-    data_path=f"{os.environ['HOME']}/data/corteconstitucional"
-
-    download_path = f"{data_path}/edictos"
     wd = build_chrome_driver(download_path, headless=True)
     wd.get(url)
-
     soup = BeautifulSoup(wd.page_source, features="html.parser")
     links = soup.find_all("a")
-    pdfs = list(set([x.attrs["href"] for x in links if x.attrs["href"].endswith(".pdf")]))
+    pdfs = list(
+        set([x.attrs["href"] for x in links if x.attrs["href"].endswith(".pdf")])
+    )
     for pdf in tqdm(pdfs):
         wd.get(f"https://www.corteconstitucional.gov.co/{pdf}")
-
-    os.system(f"ls {download_path} | wc -l") #number of documents
-
+    os.system(f"ls {download_path} | wc -l")  # number of documents
     """
     100%|██████████| 149/149 [01:00<00:00,  2.47it/s]
     148
     """
+
+
+if __name__ == "__main__":
+    download_edictos_pdfs()

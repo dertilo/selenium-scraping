cdf52e3dfe6a49ca1d8fad4c7122f7e9ce09e2cd
Tilo Himmelsbach <dertilo@gmail.com>
2020-12-28 22:24:15 +0100

    no leading zeros

--- corteconstitucional/build_csv.py
+++ corteconstitucional/build_csv.py
@@ -1,14 +1,25 @@
 from collections import defaultdict, Counter
+from datetime import datetime
+
 from pprint import pprint
 
 import pandas
 from util import data_io
 
+from corteconstitucional.parse_edictos import reformat_date
 from corteconstitucional.parse_proceso_tables import ACTUACION_SECRETARIA
+from corteconstitucional.regexes import MESES, MESES_ESP
 
 fijacion = "Fallo.Fijación Edicto"
 aprobacion = "Fallo.Aprobación Proyecto"
 radicacion = "Radicación"
+
+ANO = "Año"
+NO_EDICTO = "Nro. Edicto"
+def no_leading_zeros(s):
+    while s.startswith("0"):
+        s = s[1:]
+    return s
 
 def flatten_expedientes(d):
     tables = d.pop("tables")
@@ -21,13 +32,32 @@ def flatten_expedientes(d):
                 actuacion = f[ACTUACION_SECRETARIA]
                 dd[etapa].append(actuacion)
         dd[radicacion] = dd[radicacion][0]
-        if all([k in dd for k in [fijacion,aprobacion,radicacion]]):
-            yield {**d,**dd,**{"expediente":exp}}
+
+        datum = {**d, **dd, **{"expediente": exp}}
+        edicto_date = datetime.strptime(datum["edicto_date"], "%m/%d/%Y")
+        year = int(datum["edicto_year"])
+        mes_name = MESES_ESP[edicto_date.month-1].capitalize()
+        proceso,expediente = datum["expediente"].split("-")
+
+        tati_datum = {
+            ANO:year,
+            "Mes": mes_name,
+            NO_EDICTO: datum["no"],
+            "Proceso": proceso ,
+            "Expediente":no_leading_zeros(expediente),
+            "Sentencia": datum["sentencia"],
+            "Fecha Radicación":reformat_date(datum[radicacion]),
+            "Fecha Decisión":datum["sentencia_date"],
+            "Fijación Edicto": reformat_date(datum["edicto_date"]),
+
+        }
+        yield tati_datum
 
 
 def build_dataframe(merged_data):
     rows = (r for d in merged_data
-            for r in flatten_expedientes(d))
+            for r in flatten_expedientes(d)
+            if r[ANO]>=2015)
     # pprint(Counter(f"{r['no']}_{r['edicto_year']}" for r in rows))
     df = pandas.DataFrame(rows)
     return df
@@ -35,7 +65,9 @@ def build_dataframe(merged_data):
 
 if __name__ == '__main__':
     merged_data = list(data_io.read_jsonl("/tmp/merged_edictos2tables.jsonl"))
-
     df = build_dataframe(merged_data)
-    df = df.sort_values(['edicto_year', 'no'], ascending=[True, True])
-    df.to_csv("/tmp/table.csv",sep="\t",index=False)
+    df = df.sort_values([ANO, NO_EDICTO], ascending=[True, True])
+    consecutive_edicto_no_step = df[df[NO_EDICTO].diff() > 1]
+    for _,d in consecutive_edicto_no_step.iterrows():
+        print(f"year: {d[ANO]}; no: {d[NO_EDICTO]-1}")
+    df.to_csv("tilo_table.csv",sep="\t",index=False)
